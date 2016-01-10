5500eaa39610a932c8fc997866b5e3011488e8c2
Alex Verboon <alex.verboon@hotmail.com>
2016-01-10 17:56:53 +0100

    Added various code for gp logging

---
+++ GroupPolicy/enable_gplogging.ps1
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+# Check if running as Administrator
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+
+
+$usermode = $env:windir + "\debug\usermode"
+#Check if the usermode folder exists
+If(!(test-path -Path $usermode))
+{
+    New-Item -path  $env:windir"\debug" -ItemType Directory -ErrorAction SilentlyContinue | out-null
+    New-Item -Path $usermode -ItemType Directory | out-null 
+}
+
+#New registry key and value
+$regpath =  "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+New-Item -path $regpath -name "Diagnostics" -ErrorAction SilentlyContinue | Out-Null
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Diagnostics" -Name "GPSvcDebugLevel" -PropertyType DWORD -Value 196610 -ErrorAction SilentlyContinue | Out-Null
+
+
+
+
+
---
+++ GroupPolicy/get-gpologgingsettings.ps1
@@ -0,0 +1,63 @@
+﻿
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+   http://blogs.technet.com/b/askds/archive/2015/04/17/a-treatise-on-group-policy-troubleshooting-now-with-gpsvc-log-analysis.aspx
+#>
+function Get-GPLoggingStatus
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Computer
+    )
+
+    Begin
+    {
+        $GPLoggingRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Diagnostics"
+        $GpLoggingRegKey = "GPSvcDebugLevel"
+
+    }
+    Process
+    {
+        # GroupPolicy Logging Status
+        $GPLoggingRegStatus = Get-ItemProperty -Path $GPLoggingRegPath -Name $GpLoggingRegKey | Select-Object -ExpandProperty GPSvcDebugLevel
+        If ($GPLoggingRegStatus -eq $null -or $GPLoggingRegStatus -eq 0)
+        {
+            Write-Output "Group Policy Service Debug logging is not enabled"
+        }  
+        Else
+        {
+            $GPLoggingRegStatus = '{0:x}' -f $GPLoggingRegStatus
+            if ($GPLoggingRegStatus -eq "30002")
+            {
+                Write-Output "Group Policy Service Debug logging is enabled"
+            }
+            Else
+            {
+                Write-Output "GPSvcDebugLevel contains Unsupported value"
+            }
+         }
+    }
+
+
+
+
+
+
+
+    End
+    {
+    }
+}
+
