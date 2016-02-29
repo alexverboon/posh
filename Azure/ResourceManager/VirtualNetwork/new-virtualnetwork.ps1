5537db5c5f2cc277d7281688c5d87a4c31df3ca1
Alex Verboon <alex.verboon@hotmail.com>
2016-02-29 22:00:42 +0100

    no message

---
+++ Azure/ResourceManager/VirtualNetwork/new-virtualnetwork.ps1
@@ -0,0 +1,23 @@
+﻿
+
+
+$TemplateFile =  'azuredeploy_virtualnetwork.json'
+$ParameterFile = 'azuredeploy_virtualnetwork.parameters.json'
+
+$TemplateFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateFile)
+$ParameterFile = [System.IO.Path]::Combine($PSScriptRoot, $ParameterFile)
+
+
+Get-Content -Path $TemplateFile
+
+
+#New-AzureRmResourceGroupDeployment -Name "myvnetwork1" -ResourceGroupName "RG_2" -TemplateFile D:\DEV\azuredeploy.json -TemplateParameterFile D:\DEV\azuredeploy.parameters
+
+#$obj = @{
+#vnetName = "wow"
+#}
+
+#New-AzureRmResourceGroupDeployment -Name "myvnetwork1" -ResourceGroupName "RG_2" -TemplateFile D:\DEV\azuredeploy.json -TemplateParameterObject $obj
+
+#New-AzureRmResourceGroupDeployment -Name "myvnetwork1" -ResourceGroupName "RG_2" -TemplateFile D:\DEV\azuredeploy.json -vnetName 
+
