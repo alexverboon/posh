b1ed3179b8b1075e595cd170a6499cfb6322075f
Alex Verboon <alex.verboon@hotmail.com>
2016-04-25 22:18:41 +0200

    Added Get-Office365AccountSkuLicensedUsers cmdlet

--- Office365/README.md
+++ Office365/README.md
@@ -5,4 +5,7 @@ Here's were I keep my Office 365 Scripts
 ## Disable-MsolUserServicePlan.ps1
 The Disable-MsolUserServicePlan cmdlet disables an Office 365 Service Plan for the specified user. 
 
+## Get-Office365AccountSkuLicensedUsers
+The Get-Office365AccountSkuLicensedUsers cmdlet retrieves the users that have a specific Office 365
+subscription (AccountSkuId) assigned. 
 
---
+++ Office365/Utilities/Get-Office365AccountSkuLicensedUsers.ps1
@@ -0,0 +1,63 @@
+﻿<#
+.Synopsis
+   Get-Office365AccountSkuLicensedUsers
+.DESCRIPTION
+   The Get-Office365AccountSkuLicensedUsers cmdlet retrieves the users that have a specific Office 365
+   subscription (AccountSkuId) assigned. 
+.EXAMPLE
+   Get-Office365LicenseInfo
+
+.PARAMETER AccountSkuId
+ The name of an Office 365 AccountSkuId
+
+ A list of known AccountSkuId's pre-populated. 
+
+ Extend the list if needed, a list of known AccountSkuIDs can be found here:
+ # http://blogs.technet.com/b/treycarlee/archive/2013/11/01/list-of-powershell-licensing-sku-s-for-office-365.aspx
+ 
+ Other usefull sources related to licensing
+ # https://technet.microsoft.com/en-us/library/dn771771.aspx
+ # https://technet.microsoft.com/en-us/library/dn771773.aspx
+
+#>
+function Get-Office365AccountSkuLicensedUsers
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # AccountSkuId
+        [Parameter(Mandatory=$true,
+                   ParameterSetName = "AccountSkuId",
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [ValidateSet("ENTERPRISEPACK","RIGHTSMANAGEMENT","AAD_PREMIUM","PLANNERSTANDALONE","POWER_BI_STANDARD")]
+        $AccountSkuId
+    )
+
+    Begin
+    {
+ 
+    }
+    Process
+    {
+        If ($PSBoundParameters.ContainsKey("AccountSkuId"))
+        {
+            $AccountSkuIdUsers = Get-MsolUser | Select-Object DisplayName,UserPrincipalName -ExpandProperty Licenses |  Where-Object {$_.AccountSkuId -like "*$AccountSkuId*"}
+            $AccountSkuIdUsers | Select-Object DisplayName,UserPrincipalName
+        }
+    }
+    End
+    {
+
+    }
+}
+
+
+
+
+
+
+
+
+
