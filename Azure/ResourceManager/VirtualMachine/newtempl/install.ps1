243b0a4d33554530e15a52f209afb68809af25b3
Alex Verboon <alex.verboon@hotmail.com>
2016-05-24 01:52:33 +0200

    new template and domain join content

---
+++ Azure/ResourceManager/VirtualMachine/newtempl/install.ps1
@@ -0,0 +1,11 @@
+﻿
+$ResourceGroupName = "RG_2" 
+$Location = "westeurope" 
+$TemplateFile = "C:\Data\dev\posh\Azure\ResourceManager\VirtualMachine\newtempl\template.json"
+$ParameterFile = "C:\Data\dev\posh\Azure\ResourceManager\VirtualMachine\newtempl\parameters.json"
+
+
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFile -TemplateParameterUri $ParameterFile -Verbose
+New-AzureRmResourceGroupDeployment -Name "Deployvm_01"  -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFile -TemplateParameterUri $ParameterFile -Verbose
+
+
