c7313f49410ea7d7435e26143454839315b95bad
Alex Verboon <alex.verboon@hotmail.com>
2016-05-24 08:09:13 +0200

    no message

--- Azure/ResourceManager/VirtualMachine/newtempl/joindomain.ps1
+++ Azure/ResourceManager/VirtualMachine/newtempl/joindomain.ps1
@@ -6,6 +6,6 @@ $string1 = '{
 "Options":  "3",
 "OUPath": ""
 }'
-$string2 = '{"Password": "Holland123"}'
+$string2 = '{"Password": "password"}'
 
 Set-AzureRmVMExtension -ResourceGroupName "RG_2" -ExtensionType "JsonADDomainExtension" -Name "joindomain" -Publisher "Microsoft.Compute" -TypeHandlerVersion "1.0" -VMName "vm73" -Location "westeurope" -SettingString $string1 -ProtectedSettingString $string2
