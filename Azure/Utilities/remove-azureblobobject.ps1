43c153b7fedf4bc98ac3a43f9ceeb71051b99ae3
Alex Verboon <alex.verboon@hotmail.com>
2016-10-01 18:38:19 +0200

    update

---
+++ Azure/Utilities/remove-azureblobobject.ps1
@@ -0,0 +1,6 @@
+﻿
+
+# $ctx = New-AzureStorageContext -StorageAccountName rg11099 -StorageAccountKey ($b.StorageAccountKey)[0].Value
+ #Remove-AzureStorageBlob -Blob vm1.e3c7d72a-cc99-4946-8be7-9fd624feed68.screenshot.bmp -Container bootdiagnostics-vm1-e3c7d72a-cc99-4946-8be7-9fd624feed68  -Context $ctx
+
+ #Remove-AzureStorageContainer bootdiagnostics-vm1-e3c7d72a-cc99-4946-8be7-9fd624feed68 -Context $ctx
