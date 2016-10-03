4c1cd6e18a00ffdb5c33e24a6c4cdc148057c220
Alex Verboon <alex.verboon@hotmail.com>
2016-10-04 01:01:19 +0200

    Code snippets for OMS - Upgrade Analytics

---
+++ OMS/UpgradeAnalytics/GetWindowsUpgradeAnalyticsdata.ps1
@@ -0,0 +1,76 @@
+﻿# This script contains some examples how to retrieve Windows Upgrade Analytics data
+# that is stored in OMS
+
+# I used the guidance provided here:
+# https://blogs.technet.microsoft.com/privatecloud/2016/04/05/using-the-oms-search-api-with-native-powershell-cmdlets/
+
+# Module Installation instructions, if not installed yet.
+# Install OMS PowerShell Module
+# Find-Module AzureRM.OperationalInsights | Install-Module
+# Install-Module AzureRM.OperationalInsights -Scope AllUsers
+# Get-Module AzureRm.OperationalInsights 
+
+# Find OMS Workspaces
+Find-AzureRmResource -ResourceType "Microsoft.OperationalInsights/workspaces"
+
+$ResourceGroupName = "mms-weu"
+$WorkSpaceName = "AlexVerboonOMS" 
+
+# Get Saved Searches 
+$query = Get-AzureRmOperationalInsightsSavedSearch `
+ -ResourceGroupName $ResourceGroupName `
+ -WorkspaceName $WorkSpaceName
+$query.value |FL
+
+$query = Get-AzureRmOperationalInsightsSavedSearch `
+ -ResourceGroupName $ResourceGroupName `
+ -WorkspaceName $WorkSpaceName `
+ -SavedSearchId "test|Drivers"
+$query.properties | FL
+
+# saved search 1
+$result = Get-AzureRmOperationalInsightsSavedSearchResults `
+-ResourceGroupName $ResourceGroupName `
+-WorkspaceName $WorkSpaceName `
+-SavedSearchId "test|upgra_search1"
+$Apps = $result.value | ConvertFrom-Json
+
+# saved search 2
+$result = Get-AzureRmOperationalInsightsSavedSearchResults `
+-ResourceGroupName $ResourceGroupName `
+-WorkspaceName $WorkSpaceName `
+-SavedSearchId "test|Drivers"
+$Drivers = $result.value | ConvertFrom-Json
+
+# A custom query
+$dynamicQuery = "Manufacturer=*"
+$result = Get-AzureRmOperationalInsightsSearchResults -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkSpaceName -Query $dynamicQuery -Top 10
+$result.Value | ConvertFrom-Json
+
+
+$schema = Get-AzureRmOperationalInsightsSchema -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkSpaceName
+$schemas = $schema.Value | Select-Object Name| Sort-Object Name
+
+
+$datainfo = @()
+ForEach($s in $schemas)
+{
+    $dynamicQuery = "$($s.name)=*"
+    $result = Get-AzureRmOperationalInsightsSearchResults -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkSpaceName -Query $dynamicQuery -Top 10
+    $hasdata = $result.Value | ConvertFrom-Json
+    If ($hasdata -eq $null)
+        {
+            write-host "class $($s.name) has no log data" -ForegroundColor DarkGreen
+        }
+    Else
+        {
+            write-host "class $($s.name) contains log data" -ForegroundColor Green
+            $datainfo += "$($s.Name)"
+        }
+}
+$datainfo
+
+
+
+
+
