eda8073a93dfb933de181ba5f10ea0efafe7bed0
Alex Verboon <alex.verboon@hotmail.com>
2016-10-09 15:58:58 +0200

    Added install-powersploit script, little helper script to install the PowerShell PowerSploit module without defender interfering

---
+++ Security/install-powersploit.ps1
@@ -0,0 +1,16 @@
+﻿## 
+## Use the below commands to install the PowerSploit module
+##
+
+# add an exclusion path for defender, otherwise files will be removed as they look like malware
+# well, in fact they could be right :-)
+Set-MpPreference -ExclusionPath "C:\Program Files\WindowsPowerShell\Modules\PowerSploit\"
+# Disable defender
+Set-MpPreference -DisableRealtimeMonitoring $true
+# check the exclusionpath , should include the path set previously
+Get-MpPreference | Select-Object -ExpandProperty Exclusionpath
+# install the powersploit package
+find-package powersploit | Install-Package
+# Turn on defender again
+Set-MpPreference -DisableRealtimeMonitoring $false
+
