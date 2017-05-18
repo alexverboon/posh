5eb245f411187e1529d336d202a737958962810b
Alex Verboon <alex.verboon@hotmail.com>
2017-05-18 22:51:31 +0200

    no message

---
+++ Windows10/deviceguard.ps1
@@ -0,0 +1,15 @@
+﻿
+Get-CimInstance -Namespace root/Microsoft/Windows/DeviceGuard -ClassName Win32_DeviceGuard
+
+AvailableSecurityProperties                  : {1, 3}
+CodeIntegrityPolicyEnforcementStatus         : 0
+InstanceIdentifier                           : 4ff40742-2649-41b8-bdd1-e80fad1c
+                                               ce80
+RequiredSecurityProperties                   : {0}
+SecurityServicesConfigured                   : {0}
+SecurityServicesRunning                      : {0}
+UsermodeCodeIntegrityPolicyEnforcementStatus : 0
+Version                                      : 1.0
+VirtualizationBasedSecurityStatus            : 2
+PSComputerName                               : 
+
