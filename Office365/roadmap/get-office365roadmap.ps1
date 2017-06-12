c28753de0f4cba387200d0e00827a6073992d08e
DESKTOP-VFEKP5F\alexv <verboonalex@gmail>
2017-06-12 23:36:29 +0200

    no message

---
+++ Office365/roadmap/get-office365roadmap.ps1
@@ -0,0 +1,17 @@
+﻿
+
+Function Get-Office365Roadmap {
+    $roadmapfeatures = Invoke-WebRequest -Uri https://roadmap-api.azurewebsites.net/api/features
+    $features = $roadmapfeatures.Content
+    $rm = ($features) -join "`n" | ConvertFrom-Json 
+    $rm
+}
+
+
+
+
+
+
+
+   
+
