d7d434fffda32f2fd4acd2b7bda0ef9dbb9599df
DESKTOP-VFEKP5F\alexv <verboonalex@gmail>
2017-06-12 23:46:07 +0200

    Updated function

--- Office365/roadmap/get-office365roadmap.ps1
+++ Office365/roadmap/get-office365roadmap.ps1
@@ -1,10 +1,53 @@
 ﻿
-
 Function Get-Office365Roadmap {
-    $roadmapfeatures = Invoke-WebRequest -Uri https://roadmap-api.azurewebsites.net/api/features
-    $features = $roadmapfeatures.Content
-    $rm = ($features) -join "`n" | ConvertFrom-Json 
-    $rm
+
+<#
+.Synopsis
+  Get-Office365Roadmap
+.DESCRIPTION
+   Get-Office365Roadmap retrieves Office 365 information
+.EXAMPLE
+  Get-Office365Roadmap
+
+  Retrieves the complete Office 365 roadmap information
+
+.EXAMPLE 
+ Get-Office365Roadmap -Stats
+
+ Count Name               
+----- ----               
+  203 In development     
+  191 Launched           
+   17 Previously released
+   61 Rolling out        
+    3 Cancelled    
+
+.PARAMETER Stats
+ Shows the total number of features grouped by deployment status
+
+#>
+    [CmdletBinding()]
+    Param(
+    [switch]$Stats
+    )
+
+    Begin{}
+    Process{
+        $roadmapfeatures = Invoke-WebRequest -Uri https://roadmap-api.azurewebsites.net/api/features
+        $features = $roadmapfeatures.Content
+        $rm = ($features) -join "`n" | ConvertFrom-Json 
+    }
+    
+    End{
+        If ($Stats -eq $true)
+        {
+            $rm | Group-Object Status | Select-Object Count,Name
+        }
+        Else
+        {
+            $rm
+        }
+    }
 }
 
 
