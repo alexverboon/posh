423690b72f11c2a7ddaf4fe2923a847bfef6e7af
DESKTOP-VFEKP5F\alexv <verboonalex@gmail>
2017-09-06 23:04:44 +0200

    no message

---
+++ anothertest.ps1
@@ -0,0 +1,17 @@
+﻿
+
+ForEach ($Event in $events) {            
+    # Convert the event to XML            
+    $eventXML = [xml]$Event.ToXml()            
+    # Iterate through each one of the XML message properties            
+    For ($i=0; $i -lt $eventXML.Event.EventData.Data.Count; $i++) {            
+        # Append these as object properties    
+        
+        write-host $eventXML.Event.EventData.Data[$i].name 
+        write-host $eventXML.Event.EventData.Data[$i].'#text' 
+
+        
+                
+        #Add-Member -InputObject $Event -MemberType NoteProperty -Name  $eventXML.Event.EventData.Data[$i].name -Value $eventXML.Event.EventData.Data[$i].'#text' -Force
+    }            
+} 
