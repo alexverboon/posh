6eb668cb39f61eb5588093540db242d725f7f812
Alex <verboonalex@gmail>
2018-02-10 00:01:39 +0100

    friday stuff

---
+++ Azure/AzurePolicies/EnumPolicies.ps1
@@ -0,0 +1,64 @@
+﻿
+Function Get-xAzureRmPolicySetDefinitionDetails
+{
+<#
+
+#>
+
+[CmdletBinding()]
+
+Param()
+
+Begin{
+
+    Try{
+    $AzPolSetDef = Get-AzureRmPolicySetDefinition 
+    }
+    Catch
+    {
+        Write-error "Unable to retrieve Azure Policy Definitions"
+        Throw
+    }
+
+}
+
+Process{
+    ForEach ($PolSet in $AzPolSetDef)
+    {
+        Write-Verbose "Processing $($polset.displayName)"
+
+        # Get all all PolicyDefintiions included in the PolicySet
+        $includedpoldef = ($PolSet.Properties.policyDefinitions).policyDefinitionId
+    
+        $Result = @()
+        ForEach ($Azpoldef in $includedpoldef)
+        {
+            $def = Get-AzureRmPolicyDefinition -Id $Azpoldef 
+        
+            $object = [ordered] @{
+            PolicySetDefName = $PolSet.Name
+            PolicySetDefID = $Polset.PolicySetDefinitionId
+            PolicySetDefDisplayName = $Polset.Properties.displayName
+            PolicySetDefResourceID = $polset.ResourceId
+            PolicyDefID = $def.PolicyDefinitionId
+            PolicyDefResourceID = $def.ResourceId
+            PolicyName = $def.Name
+            PolicyID = $def.PolicyDefinitionId
+            PolicyDescription = $def.Properties.description
+            PolicyDisplayName = $def.Properties.displayName
+            PolicyCategory = $def.Properties.metadata.category
+            PolicyMode = $def.Properties.mode
+            PolicyParam = $def.Properties.parameters
+            PolicyRuleIf = $def.Properties.policyRule.if
+            PolicyRuleThen = $def.Properties.policyRule.then
+            PolicyType = $def.Properties.policyType
+        }
+        $Result += (New-Object -TypeName PSObject -Property $object)
+       }
+    }
+}
+
+End{
+    $Result
+}
+}
---
+++ Azure/AzurePolicies/FirstSteps.ps1
@@ -0,0 +1,26 @@
+﻿# Azure Policy - First Steps
+
+# https://docs.microsoft.com/en-us/azure/azure-policy/azure-policy-introduction
+
+# available PowerShell cmdlets
+get-command -Module AzureRM.Resources -Name *Policy*
+
+<#
+Get-AzureRmPolicyAssignment      
+Get-AzureRmPolicyDefinition      
+Get-AzureRmPolicySetDefinition   
+New-AzureRmPolicyAssignment      
+New-AzureRmPolicyDefinition      
+New-AzureRmPolicySetDefinition   
+Remove-AzureRmPolicyAssignment   
+Remove-AzureRmPolicyDefinition   
+Remove-AzureRmPolicySetDefinition
+Set-AzureRmPolicyAssignment      
+Set-AzureRmPolicyDefinition      
+Set-AzureRmPolicySetDefinition   
+#>
+
+# Examples Repo
+#https://github.com/Azure/azure-policy
+
+
