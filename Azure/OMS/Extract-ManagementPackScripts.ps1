0c77f1f0992f365e6011c3d090952ce22a92d961
Alex <verboonalex@gmail>
2018-02-19 19:24:51 +0100

    added OMS Security Baxseline Scripts

---
+++ Azure/OMS/Extract-ManagementPackScripts.ps1
@@ -0,0 +1,98 @@
+﻿Function Extract-ManagementPackScrpts
+{
+<#
+.SYNOPSIS
+    Extract-ManagementPackScrpts
+.DESCRIPTION
+    The Extract-ManagementPackScrpts cmdlet parses through the 
+    ManagementPack xml definition files and extracts Code that
+    is embedded within the 'ScriptBody" Node. 
+.PARAMETER ScriptOutFldr
+    Location where found script content is saved to. 
+
+.EXAMPLE
+    Extract-ManagementPackScrpts
+
+    The above command parses through all Management Pack
+    XML definition files located in the folder
+    C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State\Management Packs
+    and saves the found script content to C:\TEMP\ScrOut
+
+.NOTES
+    The extracted scripts will not run because there's still
+    some HTML code within the scripts, but you can popen the 
+    scripts in VSCODE to study the content. 
+#>
+
+
+[CmdletBinding()]
+Param(
+# Output folder to save found script content
+[string]$ScriptOutFldr = "C:\TEMP\ScrOut"
+)
+
+# Location of OMS Management Packs
+$ManagementPackFldr = "C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State\Management Packs"
+
+
+
+If ((Test-Path "$ManagementPackFldr") -eq $false)
+{
+    Write-Warning "Management Pack Folder: $ManagementPackFldr not found"
+    Throw
+}
+
+
+If ((Test-Path "$ScriptOutFldr") -eq $false )
+{
+    New-Item "$ScriptOutFldr" -ItemType Directory
+}
+
+
+$files = Get-ChildItem "$ManagementPackFldr\*.xml" -Recurse
+$Result = @()
+
+foreach ( $file in $files )
+{
+    Write-verbose "Processing $($file.Name)" 
+    [System.Xml.XmlDocument]$doc = new-object System.Xml.XmlDocument;
+    $doc.set_PreserveWhiteSpace( $true );
+    $doc.Load( $file );
+    $root = $doc.get_DocumentElement();
+    $xml = $root.get_outerXml()
+    $xml = '<?xml version="1.0" encoding="utf-8"?>' + $xml
+    $newFile = "$env:temp\$($file.Name)" + ".new"
+
+    Set-Content -Encoding UTF8 $newFile $xml 
+ 
+        # find ScriptBody Nodes
+        $scripts = Select-Xml -Path $newFile -XPath "//ScriptBody"
+        $scount=1
+        ForEach ($script in $scripts)
+        {
+            # ScriptBody
+            $ScriptBody = $script.Node.InnerXml
+            If ($ScriptBody -ne "`$Config/ScriptBody$")
+            {
+                $object = @{
+                ManagementPack = $File.Name
+                ScriptBody = $ScriptBody
+                }
+            $Result += (New-Object -TypeName PSObject -Property $object)
+            
+            $object.ScriptBody | Out-File -FilePath ("$ScriptOutFldr\$($file.name)_$scount"+".ps1") -Encoding unicode -NoNewline
+            $scount++
+            }
+        }
+    Remove-Item -Path $newFile -Force
+}
+
+Write-output "Scripts found in the following Management Packs"
+write-output ""
+
+$Result | Select-Object ManagementPack
+
+write-output ""
+Write-output "Script sources saved to $ScriptOutFldr" 
+
+}
---
+++ Azure/OMS/Get-OMSOSBaselineDefinitions.ps1
@@ -0,0 +1,102 @@
+﻿
+Function Get-OMSOSBaselineDefinitions {
+<#
+.SYNOPSIS
+    Get-OMSOSBaselineDefinitions
+.DESCRIPTION
+    Get-OMSOSBaselineDefinitions lists Configuration data from the selected
+    OMS Security configuration baseline definition file. 
+
+    These files are stored wihtin subfolders under: 
+    "C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State\Resources"
+
+.PARAMETER Baseline
+    The filename of the OMS Security Configuration Baseline 
+
+.EXAMPLE
+    Get-OMSOSBaselineDefinitions -Baseline 'C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State\Resources\689\BaselineWindowsServer2016.xml'
+
+    The above command lists all the Security Configuration Baseline definitions for
+    the Windows Server 2016 operating system. 
+
+.NOTES
+    v1.0, 19.02.2018, alex verboon
+#>
+[CmdletBinding()]
+Param(
+ )
+
+ DynamicParam {
+        
+            # Set the dynamic parameters' name
+            $ParameterName = 'Baseline'
+            # Create the dictionary 
+            $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+            # Create the collection of attributes
+            $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            # Create and set the parameters' attributes
+            $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $ParameterAttribute.Mandatory = $true
+            #$ParameterAttribute.Position = 3
+            # Add the attributes to the attributes collection
+            $AttributeCollection.Add($ParameterAttribute)
+            # Generate and set the ValidateSet 
+            $BaseLinePath = "C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State\Resources"
+            $arrSet = Get-ChildItem -Path "$BaseLinePath\*BaseLine*.xml" -Recurse -Depth 2
+            #$arrSet = Get-ChildItem -Path "$CISCATPath\CIS-CAT-FULL\Benchmarks" -Filter "*.xml"
+            $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+
+            # Add the ValidateSet to the attributes collection
+            $AttributeCollection.Add($ValidateSetAttribute)
+
+            # Create and return the dynamic parameter
+            $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+            $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+            return $RuntimeParameterDictionary
+        }
+
+Begin{
+  Write-verbose "Selected Baseline: $($PSBoundParameters["Baseline"])"
+}
+
+Process{
+    $blfile = "$($PSBoundParameters["Baseline"])"
+    $Result = @()
+
+    write-host "$($blfile.name)"
+    If (-not ($blfile.name -eq "WebBaseLineRules.xml"))
+    {
+        $Baselines = Select-xml -Path $blfile -XPath "//BaselineRegistryRule"
+    }
+    Elseif ($blfile.Name -eq "WebBaseLineRules.xml")
+    {
+        $Baselines = Select-xml -Path $blfile -XPath "//WebBaselineRule"
+    }
+
+    ForEach ($BRule in $baselines.node)
+    {
+        $object = [ordered] @{
+        BaselineFile = $blfile
+        BaselineId  = $BRule.BaselineId     
+        Id = $BRule.Id         
+        OriginalId = $BRule.OriginalId   
+        CceId = $BRule.CceId
+        Name = $BRule.Name
+        Type = $BRule.Type
+        ExpectedValue = $BRule.ExpectedValue
+        Severity = $BRule.Severity
+        AnalyzeOperation = $BRule.AnalyzeOperation
+        Enabled = $BRule.Enabled
+        AuditPolicyId = $BRule.AuditPolicyId
+        }
+        $Result += (New-Object -TypeName PSOBJECT -Property $object)
+    }
+}
+
+End{
+    $Result
+}
+}
+
+
+
