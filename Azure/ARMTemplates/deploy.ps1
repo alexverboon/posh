bf8457b00b2c19f302edf64ae0f92a9c47a1e751
Alex <verboonalex@gmail>
2018-03-18 20:10:21 +0100

    learn arm

---
+++ Azure/ARMTemplates/deploy.ps1
@@ -0,0 +1,11 @@
+
+# https://azurecitadel.github.io/workshops/arm/arm-lab1-firstTemplate/
+
+
+parms="storageAccount=saalever233 accountType=Standard_LRS"
+
+$rg="lab1"
+$template="C:\dev\posh\Azure\ARMTemplates\azuredeploy.json"
+$job="job2"
+$storageAccount="saalever233"
+
