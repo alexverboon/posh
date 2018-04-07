8718da110683388ca77f2f800f7b07b35a9de1be
Alex Verboon <verboonalex@gmail.com>
2018-04-07 13:03:03 +0200

    just stuff

---
+++ Security/CQureCourseNotes/RecoverFiles.ps1
@@ -0,0 +1,3 @@
+# How to Recover Deleted Files from the Drive
+# https://cqureacademy.com/blog/forensics/how-to-recover-deleted-files-from-the-drive
+
---
+++ Various/localposhrepo.ps1
@@ -0,0 +1,23 @@
+﻿
+# https://kevinmarquette.github.io/2017-05-30-Powershell-your-first-PSScript-repository/
+
+
+# Local PowerShell Repo
+$Path = '\\alve01\PoshRepo'
+
+$repo = @{
+    Name = 'MyRepository'
+    SourceLocation = $Path
+    PublishLocation = $Path
+    InstallationPolicy = 'Trusted'
+}
+Register-PSRepository @repo
+
+$env:PSModulePath
+
+Publish-Module -Name GroupPolicyxTended -Repository "MyRepository" 
+
+
+
+
+
